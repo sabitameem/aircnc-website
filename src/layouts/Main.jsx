d685b5fe9eadd9759cc3467ee3ee8e3d594f37d8
sabitameem <sabitameem578@gmail.com>
2023-07-16 17:44:10 +0530

    added navbar

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/shared/navbar/Navbar";
+
 const Main = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <h1>Welcome TO AirCNC</h1>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Route.jsx'
+import AuthProvider from './providers/AuthProvider'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Toaster></Toaster>
+    <AuthProvider><RouterProvider router={router} /></AuthProvider>
   </React.StrictMode>,
 )
