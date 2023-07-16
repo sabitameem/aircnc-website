c8cf96972fb8f8c389260efc5cc6be192576b010
sabitameem <sabitameem578@gmail.com>
2023-07-16 19:21:35 +0530

    created login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h3>Login page</h3>
+            <p>hello</p>
+        </div>
+    );
+};
+
+export default Login;
